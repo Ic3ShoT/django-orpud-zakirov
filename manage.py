a4714ffd9baed2df270dfb30f3104c93877ed31b
Aishat Zakirov <lickey8crp@gmail.com>
2023-05-14 20:46:33 +0300

    renamed project

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'musictracker.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mytimetrack.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- mytimetrack/wsgi.py
+++ mytimetrack/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for musictracker project.
+WSGI config for mytimetrack project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'musictracker.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mytimetrack.settings')
 
 application = get_wsgi_application()
