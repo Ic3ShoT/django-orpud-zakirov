cbb9d35958032ae9e6d26297f9f310a3fefbbf28
Aishat Zakirov <lickey8crp@gmail.com>
2023-05-27 23:50:25 +0300

    +admin, apps, forms

--- web/forms.py
+++ web/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib.auth import get_user_model
 
+from web.models import TimeSlot, TimeSlotTag, Holiday
+
 User = get_user_model()
 
 
@@ -16,3 +18,74 @@ class RegistrationForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ("email", "username", "password", "password2")
+
+
+class AuthForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput())
+
+
+class TimeSlotForm(forms.ModelForm):
+    def save(self, commit=True):
+        if not self.cleaned_data['end_date']:
+            self.instance.is_realtime = True
+        self.instance.user = self.initial['user']
+        return super().save(commit)
+
+    class Meta:
+        model = TimeSlot
+        fields = ('title', 'start_date', 'end_date', "image", "tags")
+        widgets = {
+            "start_date": forms.DateTimeInput(
+                attrs={"type": "datetime-local"}, format='%Y-%m-%dT%H:%M'
+            ),
+            "end_date": forms.DateTimeInput(
+                attrs={"type": "datetime-local"}, format='%Y-%m-%dT%H:%M'
+            )
+        }
+
+
+class TimeSlotTagForm(forms.ModelForm):
+    def save(self, commit=True):
+        self.instance.user = self.initial['user']
+        return super().save(commit)
+
+    class Meta:
+        model = TimeSlotTag
+        fields = ('title',)
+
+
+class HolidayForm(forms.ModelForm):
+    def save(self, commit=True):
+        self.instance.user = self.initial['user']
+        return super().save(commit)
+
+    class Meta:
+        model = Holiday
+        fields = ('date',)
+        widgets = {
+            "date": forms.DateTimeInput(attrs={"type": "date"}, format='%Y-%m-%d')
+        }
+
+
+class TimeSlotFilterForm(forms.Form):
+    search = forms.CharField(label='', widget=forms.TextInput(attrs={"placeholder": "Поиск"}), required=False)
+    is_realtime = forms.NullBooleanField(label="Реалтайм")
+    start_date = forms.DateTimeField(
+        label="От",
+        widget=forms.DateTimeInput(
+            attrs={"type": "datetime-local"}, format='%Y-%m-%dT%H:%M'
+        ),
+        required=False
+    )
+    end_date = forms.DateTimeField(
+        label="до",
+        widget=forms.DateTimeInput(
+            attrs={"type": "datetime-local"}, format='%Y-%m-%dT%H:%M'
+        ),
+        required=False
+    )
+
+
+class ImportForm(forms.Form):
+    file = forms.FileField()
