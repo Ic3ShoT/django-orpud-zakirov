a33d091f94a4781e7e912f9403c343c69bcc5de9
Aishat Zakirov <lickey8crp@gmail.com>
2023-05-28 01:12:44 +0300

    + services

--- timetracker/asgi.py
+++ timetracker/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for mytimetrack project.
+ASGI config for timetracker project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mytimetrack.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "timetracker.settings")
 
 application = get_asgi_application()
--- timetracker/urls.py
+++ timetracker/urls.py
@@ -1,4 +1,4 @@
-"""mytimetrack URL Configuration
+"""timetracker URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -15,8 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', include("web.urls")),
-    path('admin/', admin.site.urls),
+    path("", include("web.urls")),
+    path("api/", include("api.urls")),
+    path("admin/", admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
